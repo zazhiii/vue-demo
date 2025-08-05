a570412287696e9afe8110372831eeaeed9c6e18
zazhi <2065707071@qq.com>
2025-08-06 00:12:45 +0800

    feat: $nextTick

---
+++ 02_工程化开发/10_$nextTick/src/App.vue
@@ -0,0 +1,33 @@
+<!--
+  使用 $nextTick 方法来确保 DOM 更新完成后再执行一些操作。
+-->
+<template>
+  <div class="App">
+    <button @click="edit">编辑</button>
+    <input type="text" ref="input" v-show="visible"/>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data(){
+    return {
+      visible: false
+    }
+  },
+  methods:{
+    edit(){
+      this.visible = true;
+      // 使用 $nextTick 确保 DOM 更新完成后再执行
+      this.$nextTick(() => {
+        this.$refs.input.focus();
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
